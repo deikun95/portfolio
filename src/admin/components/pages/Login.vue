9948220c09bd2610f9628534eafdaf58c1af2568
Deikun95 <deikun95@gmail.com>
2020-09-09 00:46:41 +0300

    add login layout

--- src/admin/components/pages/Login.vue
+++ src/admin/components/pages/Login.vue
@@ -1,13 +1,56 @@
 <template>
-  <h1>Login page</h1>
+  <div class="page-content">
+    <div class="login-component">
+      <div class="login-content">
+        <div class="login-card">
+          <app-input icon="user" />
+          <app-input icon="key" />
+          <div class="login-button">
+            <appButton class="auth-btn" title="Авторизоваться" @click="onClick" />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
+import card from "../Card";
+import appInput from "../input";
+import appButton from "../button";
 
-}
+export default {
+  components: {
+    card,
+    appInput,
+    appButton,
+  },
+};
 </script>
 
-<style>
-
+<style lang="postcss">
+.page-content {
+  padding: 0 !important;
+}
+.login {
+  &-card {
+    padding: 20px 40px;
+    width: 400px;
+    background-color: #fff;
+    border-radius: 5px;
+  }
+  &-component {
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  &-button {
+    text-align: center;
+    padding-top: 20px;
+  }
+}
+.auth-btn {
+  margin: 30px 0 20px;
+}
 </style>
