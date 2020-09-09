1d038c91d5da7d547761bca02fd3fb0aa0e45ef4
o.kendigelyan <ok@devspark.ru>
2020-09-09 09:51:39 +0300

    add registation

--- src/admin/components/pages/Login.vue
+++ src/admin/components/pages/Login.vue
@@ -3,13 +3,66 @@
     <div class="login-component">
       <div class="login-content">
         <form class="login-card">
-          <div class="login-title">Авторизация</div>
-          <app-input class="login-input" v-model="user.name" title="Логин" icon="user" />
-          <div class="message">{{ validation.firstError('user.name') }}</div>
-          <app-input class="login-input" v-model="user.password" title="Пароль" icon="key" />
-          <div class="message">{{ validation.firstError('user.password') }}</div>
+          <div class="auth" v-if="isAuth">
+            <div class="login-title">Авторизация</div>
+            <app-input
+              class="login-input"
+              v-model="user.name"
+              title="Логин"
+              icon="user"
+              @keyup.enter="submitHandler"
+            />
+            <div class="message">{{ validation.firstError("user.name") }}</div>
+            <app-input
+              class="login-input"
+              type="password"
+              v-model="user.password"
+              title="Пароль"
+              @keyup.enter="submitHandler"
+              icon="key"
+            />
+            <div class="message">
+              {{ validation.firstError("user.password") }}
+            </div>
+          </div>
+          <div class="register" v-else>
+            <div class="login-title">Регистрация</div>
+            <app-input
+              class="login-input"
+              v-model="user.name"
+              title="Логин"
+              icon="user"
+              @keyup.enter="submitHandler"
+            />
+            <div class="message">{{ validation.firstError("user.name") }}</div>
+            <app-input
+              class="login-input"
+              type="password"
+              v-model="user.password"
+              title="Пароль"
+              @keyup.enter="submitHandler"
+              icon="key"
+            />
+            <div class="message">
+              {{ validation.firstError("user.password") }}
+            </div>
+          </div>
           <div class="login-button">
-            <appButton class="auth-btn" @click="submitHandler" title="Войти" />
+            <div class="login-button">
+              <appButton
+                class="auth-btn"
+                @click="regHandler"
+                plain
+                title="Зарегистрироваться"
+              />
+
+              <appButton
+                v-if="!isReg"
+                class="auth-btn"
+                @click="submitHandler"
+                title="Войти"
+              />
+            </div>
           </div>
         </form>
       </div>
@@ -28,10 +81,10 @@ const Validator = SimpleVueValidator.Validator;
 export default {
   mixins: [SimpleVueValidator.mixin],
   validators: {
-    "user.name": function (value) {
+    "user.name": function(value) {
       return Validator.value(value).required("Введите логин");
     },
-    "user.password": function (value) {
+    "user.password": function(value) {
       return Validator.value(value).required("Введите пароль");
     },
   },
@@ -46,6 +99,8 @@ export default {
         name: "",
         password: "",
       },
+      isAuth: true,
+      isReg: false,
     };
   },
   methods: {
@@ -59,12 +114,29 @@ export default {
           this.$axios
             .post("https://webdev-api.loftschool.com/login", userData)
             .then((res) => {
-                const token = res.data.token
+              const token = res.data.token;
               localStorage.setItem("token", token);
               this.$axios.defaults.headers["Authorization"] = `Bearer ${token}`;
-              this.$router.replace("/")
+              this.$router.replace("/");
             })
             .catch((err) => console.log(err));
+        }
+      });
+    },
+    regHandler() {
+      this.isAuth = false;
+      const userData = {
+        name: this.user.name,
+        password: this.user.password,
+      };
+      this.$validate().then((success) => {
+        if (success) {
+          if (this.isReg === true) {
+            this.$axios.post(`${this.$baseUrl}/register`, userData).then(() => {
+              (this.isReg = false), (this.isAuth = true);
+            });
+          }
+          this.isReg = true;
         }
       });
     },
@@ -80,7 +152,7 @@ export default {
   &-card {
     display: block;
     padding: 20px 40px;
-    width: 400px;
+    width: 500px;
     background-color: #fff;
     border-radius: 5px;
   }
@@ -93,6 +165,8 @@ export default {
   &-button {
     text-align: center;
     padding-top: 20px;
+    display: flex;
+    justify-content: center;
   }
   &-input {
     margin-top: 20px;
